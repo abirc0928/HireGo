482fcd98b6a23164e37f2b1f0bf0b51773960a50
abirc0928 <abirchowdhury0928@gmail.com>
2025-01-02 22:08:28 +0600

    HireGO

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["IBM Plex Sans Hebrew", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
